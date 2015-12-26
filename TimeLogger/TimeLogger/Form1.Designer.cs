42421bac4100e72b66f50151ef766bef9195a811
RoxtonLabs <roxtonlabs@gmail.com>
2015-12-26 18:14:04 +0000

    Checks for user activity within the last minute; if activity then increases "minute worked counter" by 1.

---
+++ TimeLogger/TimeLogger/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TimeLogger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(13, 13);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(260, 13);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Location = new System.Drawing.Point(94, 18);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(160, 23);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "Press \"Start\"";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Click to show Time Logger";
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 50);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "MainForm";
+            this.Text = "Time Logger";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ TimeLogger/TimeLogger/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;   //For importing DLLs
+using System.Timers;    //For the timer (duh)
+
+namespace TimeLogger
+{
+    public partial class MainForm : Form
+    {
+        Boolean running = false;
+        System.Timers.Timer logTimer = new System.Timers.Timer(60000);    //Once a minute
+        int minutesWorked = 0;
+
+        //Import the LastInputInfo function
+        [DllImport("User32.dll")]
+        static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+
+        static uint GetLastInputTime()
+        {   //Calculates the time since last input in seconds
+            //Sourced from http://www.pinvoke.net/default.aspx/user32.GetLastInputInfo
+            uint idleTime = 0;
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            uint envTicks = (uint)Environment.TickCount;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                uint lastInputTick = lastInputInfo.dwTime;
+                idleTime = envTicks - lastInputTick;
+            }
+            return ((idleTime > 0) ? (idleTime / 1000) : 0);
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            logTimer.Enabled = false;
+            logTimer.Elapsed += new ElapsedEventHandler(logTimer_Elapsed);
+        }
+
+        void logTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {   //Called once a minute when the timer elapses
+            if (GetLastInputTime() < 60)
+            {   //If the user has done something within the last minute
+
+                //Increase the counter
+                minutesWorked += 1;
+
+                //When we have the background worker up and working update status label
+                //For now just print out
+                Console.WriteLine("User last did something " + GetLastInputTime().ToString() + " seconds ago. " + minutesWorked.ToString() + " minutes worked.");
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {   //If not running then start running
+                if (minutesWorked>0)
+                {
+                    statusLabel.Text = minutesWorked.ToString() + " minutes worked. Running...";
+                }
+                else
+                {
+                    statusLabel.Text = "Running...";
+                }
+                startButton.Text = "Stop";
+                running = true;
+                logTimer.Enabled = true;
+                logTimer.Stop();
+                logTimer.Start();
+            }
+            else
+            {
+                startButton.Text = "Start";
+                running = false;
+                logTimer.Stop();
+                logTimer.Enabled = false;
+                statusLabel.Text = minutesWorked.ToString() + " minutes worked.";
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+                Hide();     //Hide the application from the taskbar
+            }
+            else
+            {
+                notifyIcon.Visible = false;
+            }
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            Show();     //Restore the app to the taskbar
+            WindowState = FormWindowState.Normal;    //Un-minimize the app window
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            minutesWorked = 0;
+            if (running)
+            {
+                statusLabel.Text = "Restarted. Running...";
+            }
+            else
+            {
+                statusLabel.Text = "Restarted. Stopped.";
+            }
+        }
+    }
+}
